5649cedc1d9155e205d751c642d5949621885337
lichengcai <lichengcai@primeledger.cn>
2018-12-05 11:10:54 +0800

    nio demo

---
+++ io-nio-demo/tomcat-io/src/main/java/com/suimi/hello/tomcatio/TomcatIoApplication.java
@@ -0,0 +1,11 @@
+package com.suimi.hello.tomcatio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class TomcatIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomcatIoApplication.class, args);
+    }
+}
